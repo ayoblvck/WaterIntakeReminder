060f7b229e3783c7fed8beaedaa1498f34e13c4b
Maryam Sanni <60554621+ayoblvck@users.noreply.github.com>
2023-08-29 23:34:46 +0100

    update file

--- WaterReminder/server.R
+++ WaterReminder/server.R
@@ -12,8 +12,12 @@ library(shiny)
 # Define server logic required to draw a histogram
 function(input, output, session) {
 
+  congrats_shown <- reactiveVal(FALSE)
+  
   data <- reactiveValues(
-    intake = data.frame(date = character(), time = character(), intake = numeric() )
+    intake = data.frame(date = character(), time = character(), intake = numeric()),
+    last_date = Sys.Date(),
+    daily_progress = 0
   )
   
   observe({
@@ -35,14 +39,47 @@ function(input, output, session) {
    
    
    data$intake <- rbind(data$intake, new_row)
-   updateProgressBar(session, "progress", value =  sum(data$intake$intake)/data$target * 100)
+   
+   if (as.Date(data$intake[nrow(data$intake), "date"]) != data$last_date) {
+     data$last_date <- as.Date(data$intake[nrow(data$intake), "date"])
+     data$daily_progress <- 0
+   }
+   
+   data$daily_progress <- data$daily_progress + input$water_intake
+   
+   updateProgressBar(session, "progress", value =  data$daily_progress/data$target * 100)
+   
+   if (!congrats_shown() & data$daily_progress >= data$target) {
+     congrats_shown(TRUE)
+     showModal(
+       modalDialog(
+         HTML("<h4>Congratulations! You've reached your daily water intake target.</h4>
+              <img src='clapping.gif' alt='Clapping Gif' class = 'gif_image'>"),
+         actionButton("share_button", "Share Achievement on Social Media"),
+         easyClose = TRUE
+       ))
+   }
+   
+   write.csv(data$intake, "water_intake_data.csv", row.names = FALSE)
  })
  
+ #draw a graph
+ graph_visible <- reactiveVal(FALSE)
+ observeEvent(input$toggle_graph, {
+   graph_visible(!graph_visible())
+ })
+ 
+
  output$intake_plot <- renderPlot({
-   ggplot(data$intake, aes(x = as.POSIXct(date), y = intake)) +
+   if(graph_visible() && nrow(data$intake) != 0){
+   agg_data <- aggregate(intake ~ date, data$intake, sum)
+   ggplot(agg_data, aes(x = as.POSIXct(date), y = intake)) +
      geom_line() +
-     labs(x = "Date and Time", y = "Water Intake (oz)",
+     labs(x = "Date and Time", y = "Water Intake (ml)",
           title = "Water Intake Over Time")
+   } 
+     
+   
  })
  
 }
--- WaterReminder/ui.R
+++ WaterReminder/ui.R
@@ -14,13 +14,15 @@ library(ggplot2)
 
 # Define UI for application that draws a histogram
 fluidPage(
-
+    includeCSS("www/style.css"),
     # Application title
     titlePanel("Water Intake Tracker"),
+    p("Welcome to my water intake tracking app", class = "head_message"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
+          p("Select your target water intake and input the amount of water taken at a time (in milliliters)"),
           dateInput("intake_date", "Select Date: ", value = Sys.Date()),
           
           checkboxInput("use_current_time", "Use current time", value = TRUE),
@@ -28,23 +30,29 @@ fluidPage(
             condition = "!input.use_current_time",
             timeInput("intake_time", "Select Time", value = "12.00")
           ),
-          
-          numericInput(inputId = "water_intake", 
-                       label ="Input amount of water ",
-                       value = 10),
           selectInput("target_intake", 
-                      "select water intake target",
-                      choices = c("4 cups" = 50, "5 liters" = 90),
-                      selected = 50),
+                      "select water intake target in cups",
+                      choices = c("3 cups" = 709.765, "4 cups" = 946.353, "5 cups" = 1182.94, "6 cups" = 1419.53, "7 cups" = 1656.12, "8 cups" = 1892.71, "9 cups" = 2129.29, "10 cups" = 2365.88),
+                      selected = 1182.94),
+          numericInput(inputId = "water_intake", 
+                       label ="Input water taken in milliliters (1 cup = 236)",
+                       value = 236),
           actionButton("submit", "submit")
+          
         ),
         mainPanel(
           h3("Intake progress"),
           progressBar("progress", value = 0),
-          plotOutput("intake_plot"),
-          h4("Encouragement"),
-          verbatimTextOutput("encouragement")
-        )
+          actionButton("toggle_graph", "Toggle Graph"),
+          conditionalPanel(
+          condition = "input.toggle_graph > 0",
+          plotOutput("intake_plot")
+        ),
+        conditionalPanel(
+          condition = "input.toggle_graph==0",
+          p("Click the toggle graph button to view a summary graph of your water intake"))
 
         )
     )
+    
+)
